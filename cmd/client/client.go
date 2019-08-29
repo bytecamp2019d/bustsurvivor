4a656d883deff2cad673e7e0978db6151ea35263
Thomstrong <a80090@qq.com>
2019-08-29 11:13:29 +0800

    Cwq/new rule based (#3)
    * ADD: try to construct the framework code
    
    * MOD: modelize framework
    
    * fix stupid init bug
    
    * add test case config method
    
    * fix file not exist bug
    
    * add calculator
    
    * Rule based weight justify
    
    * bug fix
    
    * bug fix
    
    * debug for duration sync lock
    
    * new rule
    
    * fix weight update
    
    * 	new file:   out.log
    
    * 	modified:   out.log
    
    * 	modified:   out.log
    
    * add maxmin and pct99

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,62 +1,84 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
-	"time"
-
-	"google.golang.org/grpc"
-
 	bs "github.com/bytecamp2019d/bustsurvivor/api/bustsurvivor"
+	"github.com/bytecamp2019d/bustsurvivor/model/balancer"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
 )
 
-const (
-	connNum = 3
-	rps     = 100
-	loopNum = 500
-)
+type TestCase struct {
+	ConnNum      int `json:"connNum"`
+	Rps          int `json:"rps"`
+	LoopNum      int `json:"loopNum"`
+	NextInterval int `json:"nextInterval"`
+}
+
+type TestCases struct {
+	Cases []TestCase `json:"cases"`
+}
 
 // call .
 func call() {
-	target := "127.0.0.1:8080"
-	var totalDur time.Duration
-	durChan := make(chan time.Duration, connNum*loopNum)
-	errChan := make(chan error, connNum*loopNum)
-	for i := 0; i < connNum; i++ {
-		go func() {
-			conn, err := grpc.Dial(target, grpc.WithInsecure())
-			if err != nil {
-				panic(err)
-			}
-			defer conn.Close()
-			client := bs.NewSurvivalServiceClient(conn)
-			ctx := context.Background()
-			for j := 0; j < loopNum; j++ {
-				go func() {
+	configPath, _ := filepath.Abs("test_config.json")
+	jsonFile, err := os.Open(configPath)
+
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
+	var tcs TestCases
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	_ = json.Unmarshal(byteValue, &tcs)
+	for caseIndex, tc := range tcs.Cases {
+		fmt.Printf("============= No.%d test case ===============\n", caseIndex+1)
+		connNum := tc.ConnNum
+		rps := tc.Rps
+		loopNum := tc.LoopNum
+		nextInterval := tc.NextInterval
+
+		balancer.InitBalancer(connNum, caseIndex == 0)
+
+		totalDur := time.Duration(0)
+		durChan := make(chan time.Duration)
+		errChan := make(chan error)
+		errCnt := 0
+		for i := 0; i < connNum; i++ {
+			go func() {
+				for j := 0; j < loopNum; j++ {
 					req := &bs.BustSurvivalRequest{
 						CardsToPick:   10,
 						BustThreshold: 80,
 					}
-					begin := time.Now()
-					resp, err := client.BustSurvival(ctx, req)
-					durChan <- time.Since(begin)
-					errChan <- err
-					_ = resp // ignore resp
-				}()
-				time.Sleep(time.Second / rps)
-			}
-		}()
-		time.Sleep(time.Second / rps / connNum)
-	}
-	errCnt := 0
-	for i := 0; i < connNum*loopNum; i++ {
-		totalDur += <-durChan
-		err := <-errChan
-		if err != nil {
-			errCnt++
+					go balancer.SendRequest(req, durChan, errChan)
+					time.Sleep(time.Second / time.Duration(rps))
+				}
+			}()
+			time.Sleep(time.Second / time.Duration(rps*connNum))
 		}
+
+		for i := 0; i < connNum*loopNum; i++ {
+			totalDur += <-durChan
+			err := <-errChan
+			if err != nil {
+				errCnt++
+			}
+		}
+
+		fmt.Println("### Client side ###")
+		fmt.Printf("Average letency is %v, errRate is %.1f%%\n",
+			totalDur/time.Duration(connNum*loopNum),
+			float64(100*errCnt)/float64(connNum*loopNum),
+		)
+		fmt.Println("### Balancer side ###")
+		balancer.GetReport()
+		fmt.Printf("Next request lists will come %vs latter.\n", nextInterval)
+		time.Sleep(time.Duration(nextInterval) * time.Second)
 	}
-	fmt.Printf("Average letency is %v, errRate is %.1f%%\n", totalDur/connNum/loopNum, float64(100*errCnt)/float64(connNum*loopNum))
 }
 
 func main() {
